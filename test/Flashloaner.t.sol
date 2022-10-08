a1e160e765f2dda3650b8754b300cc0378ceb3b0
simon busch <simon.busch@live.fr>
2022-10-08 10:47:15 +0200

    update tests

--- test/Flashloaner.t.sol
+++ test/Flashloaner.t.sol
@@ -1,13 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 import "forge-std/Test.sol";
+import "solmate/test/utils/mocks/MockERC20.sol";
+import "../src/Flashloaner.sol";
 
 // forge test --match-contract FlashloanerTest -vvvv
 contract FlashloanerTest is Test {
-    address player = address(100);
+    address player1 = address(100);
+    address player2 = address(102);
+    address maliciousAddress = address(0);
+
+    MockERC20 token;
+    Flashloaner loaner;
 
     function setUp() public {
-        vm.deal(player, 5 ether);
+        vm.label(player1, "Simon");
+        vm.label(player2, "Bob");
+        vm.label(address(this), "Test contract");
+
+        token = new MockERC20("TestToken", "TT0", 18);
+        vm.label(address(token), "TestToken");
+
+        loaner = new Flashloaner(address(token));
+
+        token.mint(address(this), 1e18);
     }
 
     function testExample() public {
